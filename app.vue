3e87c5996db5f14acaa5632823b824d9acbca1a5
Samba Gueye <samba.gueye@abitare-kids.com>
2023-07-13 01:34:47 +0200

    first commit

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <nav style="display: flex; gap: 15px">
+    <nuxt-link to="/">Home</nuxt-link>
+    <nuxt-link to="/movies">Movies</nuxt-link>
+  </nav>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
---
+++ pages/teams/[teamSlug]/users/[userId].vue
@@ -0,0 +1,13 @@
+<script setup>
+const route = useRoute();
+const message = computed(
+  () =>
+    `I am on team: ${route.params.teamSlug} and my user id is: ${route.params.userId}`
+);
+</script>
+
+<template>
+  <div>{{ message }}</div>
+</template>
+
+<style scoped></style>
